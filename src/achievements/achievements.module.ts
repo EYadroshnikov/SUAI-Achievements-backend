b62287e9ff9ecc26dce0403081c2fae34e576b54
EYadroshnikov <yadegor1375@gmail.com>
2024-07-21 22:40:18 +0300

    fix: add entity into the module

--- src/achievements/achievements.module.ts
+++ src/achievements/achievements.module.ts
@@ -8,10 +8,16 @@ import { UsersModule } from '../users/users.module';
 import { User } from '../users/entities/user.entity';
 import { TelegramModule } from '../telegram/telegram.module';
 import { VkModule } from '../vk/vk.module';
+import { AchievementOperation } from './entities/achievement-operation.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Achievement, IssuedAchievement, User]),
+    TypeOrmModule.forFeature([
+      Achievement,
+      IssuedAchievement,
+      User, //TODO: check if I can remove User Entity from here
+      AchievementOperation,
+    ]),
     UsersModule,
     TelegramModule,
     VkModule,
