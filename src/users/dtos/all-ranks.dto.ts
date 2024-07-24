97defad408522510036734fd57dae8108e200160
EYadroshnikov <yadegor1375@gmail.com>
2024-07-24 12:48:13 +0300

    fix: all ranks dto swagger types

--- src/users/dtos/all-ranks.dto.ts
+++ src/users/dtos/all-ranks.dto.ts
@@ -1,5 +1,17 @@
+import { Exclude, Expose } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
+
+@Exclude()
 export class AllRanksDto {
+  @ApiProperty()
+  @Expose()
   groupRank: number;
+
+  @ApiProperty()
+  @Expose()
   instituteRank: number;
+
+  @ApiProperty()
+  @Expose()
   universityRank: number;
 }
